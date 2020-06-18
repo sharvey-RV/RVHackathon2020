739569d3f07b46087ccedabba1ae36c883496b4a
Syrsha <sharvey@redventures.com>
2020-06-18 11:10:09 -0400

    change input fields

--- src/Form.js
+++ src/Form.js
@@ -3,25 +3,24 @@ import React from 'react'
 class Form extends React.Component {
   constructor(props) {
     super();
-    //this.state = {
-	//commandID:'',
-	//commmandKeyword:'',
-	//expectedVal:''
-    //}
+    this.state = {
+	body:'',
+	command:''
+    }
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
-    //var AWS = require('aws-sdk');
     event.preventDefault();
     console.log(event.target);
-    const data = new FormData(event.target);
+    const {name, value} = event.target
+    //const data = new FormData(event.target);
+    this.setState({[name]:value});
+    const data = this.state;
     console.log("sending data to backend");
     console.log(data);
-    //send to backend
-
-    fetch('/path/to/backend', {
+    fetch('https://h6d3rqs549.execute-api.us-west-1.amazonaws.com/TestProd/addcommand',{
       method: 'POST',
       body: data,
     });
@@ -30,17 +29,13 @@ class Form extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-	<div className="commandID">
-            <label htmlFor="commandID">Enter command ID: </label>
-            <input id="commandID" name="commandID" type="text" />
- 	</div>
-	<div className="commandKeyword">
-	    <label htmlFor="commandKeyword">Enter command keyword: </label>
-            <input id="commandKeyword" name="commandKeyword" type="text" />
+	<div className="command">
+	    <label htmlFor="command">Enter command: </label>
+            <input id="command" name="command" placeholder="Command" defaultValue={this.state.command} type="text"/>
         </div>
-	<div className="expectedVal">
-	    <label htmlFor="expectedVal">Enter expected value: </label>
-            <input id="expectedVal" name="expectedVal" type="text" />
+	<div className="body">
+	    <label htmlFor="body">Enter body: </label>
+            <input id="body" name="body" placeholder="Body" defaultValue={this.state.body} type="text"/>
         </div>
 	<div className="submit">
 	    <button>Submit!</button>
