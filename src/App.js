f81488e5b2b6d6384180763632073c7e165d1a7e
Syrsha <sharvey@redventures.com>
2020-06-22 00:23:59 -0400

    just merging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import MyForm from './components/MyForm';
 import MyTable from './components/MyTable';
-import NavBar1 from './components/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/header.css';
-import redVen from './image/Red-Ventures.jpg';
-import slackLogo from './image/slacklogo.png';
 import Header from './components/header'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -19,14 +16,14 @@ class App extends Component {
     return (
       <React.Fragment>
         <Header></Header>
-
         <Router>
-
           <Switch>
             <Route exact path='/' >
               <MyForm handleSubmit={this.handleSubmit} />
             </Route>
-
+            <Route exact path='/View'>
+	      <MyTable/>
+	    </Route>
             <Route path='/about' />
           </Switch>
         </Router>
